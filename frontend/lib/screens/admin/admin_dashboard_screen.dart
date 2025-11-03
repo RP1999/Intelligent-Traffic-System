c7a171484938124b582676e54f6ec8025e819502
Tennakoon I.M.S.R <it22363848@my.sliit.lk>
2025-11-03 14:09:02 +0530

    update fuzzy logic

---
+++ frontend/lib/screens/admin/admin_dashboard_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../core/theme/app_colors.dart';
+import '../../core/theme/app_typography.dart';
+import '../../providers/auth_provider.dart';
+import '../../widgets/admin/admin_sidebar.dart';
+import '../../widgets/admin/stat_card.dart';
+import '../../widgets/admin/live_video_feed.dart';
+import '../../widgets/admin/traffic_light_panel.dart';
+import '../../core/network/api_client.dart';
+import '../../core/network/api_endpoints.dart';
+
+class AdminDashboardScreen extends StatefulWidget {
+  const AdminDashboardScreen({super.key});
+
+  @override
+  State<AdminDashboardScreen> createState() => _AdminDashboardScreenState();
+}
+
+class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
+  int _selectedIndex = 0;
+  Map<String, dynamic>? _stats;
+  bool _isLoading = true;
+  
+  final ApiClient _apiClient = ApiClient();
