750d95e52ff68a353c3a4125168174288ef7ff43
dilina randima <it22900890@my.sliit.lk>
2025-11-05 20:22:00 +0530

    Implement red light stop line visualization

--- frontend/lib/providers/admin/drivers_provider.dart
+++ frontend/lib/providers/admin/drivers_provider.dart
@@ -2,3 +2,7 @@ import 'package:flutter/foundation.dart';
 import '../../core/network/api_client.dart';
 import '../../core/network/api_endpoints.dart';
 import '../../models/driver.dart';
+
+/// State enum for data loading
+enum LoadingState { initial, loading, loaded, error }
+
