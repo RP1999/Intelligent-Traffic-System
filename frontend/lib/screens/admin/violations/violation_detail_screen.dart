3f981b3e9dc5e26b6b8d88780314594d54ffa73c
dilina randima <it22900890@my.sliit.lk>
2025-11-07 15:44:00 +0530

    Add parking zone definitions

---
+++ frontend/lib/screens/admin/violations/violation_detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import '../../../core/theme/app_colors.dart';
+import '../../../core/theme/app_typography.dart';
+import '../../../core/config/app_config.dart';
+import '../../../providers/admin/violations_provider.dart';
+import '../../../models/violation.dart';
+import '../../../widgets/common/loading_widget.dart';
+
+class ViolationDetailScreen extends StatefulWidget {
+  final String violationId;
+
+  const ViolationDetailScreen({
+    super.key,
+    required this.violationId,
