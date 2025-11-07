6ba932cd653a7fe219f88597ca520ec8c7c1e1fe
dilina randima <it22900890@my.sliit.lk>
2025-11-07 14:20:00 +0530

    Update driver risk level logic

--- frontend/lib/screens/admin/drivers/driver_detail_screen.dart
+++ frontend/lib/screens/admin/drivers/driver_detail_screen.dart
@@ -14,3 +14,19 @@ class DriverDetailScreen extends StatefulWidget {
     super.key,
     required this.driverId,
   });
+
+  @override
+  State<DriverDetailScreen> createState() => _DriverDetailScreenState();
+}
+
+class _DriverDetailScreenState extends State<DriverDetailScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<DriversProvider>().loadDriverDetail(widget.driverId);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
