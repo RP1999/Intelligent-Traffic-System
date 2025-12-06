748233bf06a6a2324e1a46566b55ee876f9a7e7c
Gunarathna RP <163238777+RP1999@users.noreply.github.com>
2025-12-06 18:46:02 +0530

    Create basic Login Screen layout

--- frontend/lib/screens/auth/driver_login_screen.dart
+++ frontend/lib/screens/auth/driver_login_screen.dart
@@ -1,1 +1,25 @@
+  import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../core/theme/app_colors.dart';
+import '../../core/theme/app_typography.dart';
+import '../../providers/auth_provider.dart';
+import '../../widgets/common/common.dart';
+
+class DriverLoginScreen extends StatefulWidget {
+  const DriverLoginScreen({super.key});
+
+  @override
+  State<DriverLoginScreen> createState() => _DriverLoginScreenState();
+}
+
+class _DriverLoginScreenState extends State<DriverLoginScreen>
+    with SingleTickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
+  final _phoneController = TextEditingController();
+  final _passwordController = TextEditingController();
   
+  late AnimationController _animController;
+  late Animation<double> _fadeAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
