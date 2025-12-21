293e1b4e24174db9c763182488080aed3829204a
Gunarathna RP <163238777+RP1999@users.noreply.github.com>
2025-12-21 10:59:33 +0530

    Setup Driver Detail screen scaffold

--- frontend/lib/screens/admin/drivers/driver_detail_screen.dart
+++ frontend/lib/screens/admin/drivers/driver_detail_screen.dart
@@ -1,1 +1,43 @@
-  
+  import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import '../../../core/theme/app_colors.dart';
+import '../../../core/theme/app_typography.dart';
+import '../../../providers/admin/drivers_provider.dart';
+import '../../../models/driver.dart';
+import '../../../widgets/common/loading_widget.dart';
+
+class DriverDetailScreen extends StatefulWidget {
+  final String driverId;
+
+  const DriverDetailScreen({
+    super.key,
+    required this.driverId,
+  });
+
+  @override
+  State<DriverDetailScreen> createState() => _DriverDetailScreenState();
+}
+
+class _DriverDetailScreenState extends State<DriverDetailScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<DriversProvider>().loadDriverDetail(widget.driverId);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        title: const Text('Driver Profile'),
+        backgroundColor: AppColors.surface,
+      ),
+      body: Consumer<DriversProvider>(
+        builder: (context, provider, _) {
+          if (provider.detailState == LoadingState.loading) {
+            return const LoadingWidget(message: 'Loading driver details...');
+          }
